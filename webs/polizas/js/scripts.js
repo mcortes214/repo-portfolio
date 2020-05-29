2ebf6fe1e0849d742ce1e06a5e48e3341b54b435
Rizoma <rizomadg@gmail.com>
2020-05-29 14:35:03 -0300

    Polizas v1

---
+++ webs/polizas/js/scripts.js
@@ -0,0 +1,28 @@
+console.log('test');
+
+var allText =[];
+var allTextLines = [];
+var Lines = [];
+
+var txtFile = new XMLHttpRequest();
+
+txtFile.open("GET", "datos-polizas.csv", true);
+txtFile.onreadystatechange = function()
+{
+    allText = txtFile.responseText;
+    allTextLines = allText.split(/\r\n|\n/);
+};
+
+// document.write(allTextLines);
+// document.write(allText);
+// document.write(txtFile);
+
+
+$(document).ready(function(){
+
+  $('#generar').click(function(){
+    var input = $('input[name="test"]').val();
+    console.log(input);
+  });
+
+});
