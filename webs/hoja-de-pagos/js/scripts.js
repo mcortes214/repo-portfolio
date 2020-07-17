2a27e37e6de34f3a49d350a3ef53f359d482d80b
Rizoma <rizomadg@gmail.com>
2020-07-17 09:59:39 -0300

    Hoja de pagos

--- webs/hoja-de-pagos/js/scripts.js
+++ webs/hoja-de-pagos/js/scripts.js
@@ -2,6 +2,45 @@ console.log('holi');
 
 $(document).ready(function(){
 
+  $('.imprimir').click(function(){
+      window.print();
+  });
 
+  $('#crear-hoja').click(function(){
+    crearFechas();
+    $('.overlay-pagina').css({'left' : '-100vw'});
+    $('.overlay-pagina').css({'opacity' : '0'});
+  });
 
 });
+
+function crearFechas() {
+  var d = $('#input-fecha').val();
+  if(d){
+    moment.locale('es');
+    var fechaDeHoy = moment(d);
+    var fechaHoyFormato = fechaDeHoy.format('LL');
+    console.log(fechaHoyFormato);
+
+    //Fecha inicial
+    $('.fecha-de-hoy').append(fechaHoyFormato);
+
+    //Fechas subsiguientes
+    var semana;
+
+    for (var i = 0; i < 8; i++) {
+       semana = fechaDeHoy.add(7, 'days');
+       $('.bloque-1').append('<p class="fecha-pago">'+semana.format('LL')+'</p>');
+    }
+
+    for (var i = 0; i < 2; i++) {
+       semana = fechaDeHoy.add(7, 'days');
+       $('.bloque-2').append('<p class="fecha-pago">'+semana.format('LL')+'</p>');
+    }
+
+    for (var i = 0; i < 2; i++) {
+       semana = fechaDeHoy.add(7, 'days');
+       $('.bloque-3').append('<p class="fecha-pago">'+semana.format('LL')+'</p>');
+    }
+  }
+}
